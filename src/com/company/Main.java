715165d6591eef2f5249bf5b908f20ecdea293fb
Ostapenko <seostapenko@gmail.com>
2021-02-28 19:24:39 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int p = 4;
+        int[] R = new int[]{4,7,9,11};
+        for (int i=0; i<R.length; i++){
+            System.out.print(R[i]+",");
+        }
+
+        System.out.println();
+
+        System.out.println("HelloWorld");
+        System.out.println("p="+p);
+        System.out.print("R="+R);
+    }
+}
